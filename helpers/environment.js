ef2016e8e04a7add977750e4d7f296944750a9e5
Samiul Karim <samiulkarimprodhan@gmail.com>
2024-04-28 15:23:23 +0600

    Update Twilio credentials in environment.js and phone number in server.js

---
+++ helpers/environment.js
@@ -0,0 +1,38 @@
+// Handle all environment related stuff here
+
+const environment = {};
+
+environment.staging = {
+  port: 3000,
+  envName: 'staging',
+  secretKey: 'samiul',
+  maxChecks: 5,
+  twilio: {
+    fromPhone: '',
+    accountSid: '',
+    authToken: '',
+  },
+};
+
+environment.production = {
+  port: 5000,
+  envName: 'production',
+  secretKey: 'karim',
+  maxChecks: 5,
+  twilio: {
+    fromPhone: '',
+    accountSid: '',
+    authToken: '',
+  },
+};
+
+// determine which environment was passed
+const currentEnvironment =
+  typeof process.env.NODE_ENV === 'string' ? process.env.NODE_ENV : 'staging';
+
+// export corresponding environment object
+const environmentToExport =
+  typeof environment[currentEnvironment] === 'object'
+    ? environment[currentEnvironment]
+    : environment.staging;
+module.exports = environmentToExport;
